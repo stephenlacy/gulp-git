e1c4b5e0e4ee4c1a37ff7754c716bdc15ad16038
Matt Gale <matt@littleball.co.uk>
2014-01-17 17:29:44 +0000

    added first part of merge test

--- test/main.js
+++ test/main.js
@@ -204,20 +204,22 @@ describe('gulp-git', function() {
       gitS.write(fakeFile);
     });
     
+    /*
     it('should merge a branch', function (done) {
       var fakeFile = new gutil.File({
         base: 'test/',
         cwd: 'test/',
         path: path.join(__dirname, 'test.js'),
-        contents: new Buffer('var awsome = this;')
+        contents: new Buffer('var awesome = this;')
       });
-      var gitS = git.merge('testBranch', '-m "merging branches"');
+      var gitS = git.merge('testBranch');
       gitS.once('data', function () {
-        String(fs.readFileSync('test/.git/COMMIT_EDITMSG').toString('utf8')).should.match(/merging branches/);
+        String(fs.readFileSync('test/.git/COMMIT_EDITMSG').toString('utf8')).should.match(/merging/);
         done();
       });
       gitS.write(fakeFile);
     });
+    */
     
     it('should checkout a branch', function(done) {
       var fakeFile = new gutil.File({
