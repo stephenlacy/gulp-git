02ae8e06b3c5bed90c292d539c5090a5d3d97c0e
Steve Lacy <me@slacy.me>
2014-04-23 11:13:47 -0700

    Clean err reporting

--- test/main.js
+++ test/main.js
@@ -49,7 +49,7 @@ describe('gulp-git', function() {
       });
     });
 
-/*
+  /*
     it('should pull from the remote repo', function(done) {
       git.pull('origin', 'master', {cwd: "./test/"}, function(){
         should.exist('./test/.git/refs/heads/master');
@@ -57,8 +57,9 @@ describe('gulp-git', function() {
       });
     });
 
-  // This must be run on a system which has git installed, and has git configured.
-*/
+    // These must be run on a system which has git installed, no pull delay, and has git configured.
+
+
     it('should tag a version of the repo', function(done) {
 
       git.tag('v1.2.3', 'message', {cwd: "./test/"}, function(){
@@ -67,6 +68,8 @@ describe('gulp-git', function() {
       });
 
     });
+  */
+
 
     it('should commit a file to the repo', function(done) {
       var fakeFile = new gutil.File({
@@ -102,6 +105,8 @@ describe('gulp-git', function() {
       });
     });
 
+    /* 
+     Requires git pull
     it('should checkout a branch', function(done) {
       var fakeFile = new gutil.File({
         base: 'test/',
@@ -117,83 +122,7 @@ describe('gulp-git', function() {
       gitS.end();
     });
 
-
-
-  // With options
-
-
-
-    it('should add files to the git repo - with options', function(done) {
-      var fakeFile = new gutil.File({
-        base: 'test/',
-        cwd: 'test/',
-        path: path.join(__dirname, '/test.js'),
-        contents: new Buffer(fs.readFileSync('test/test.js'))
-      });
-      var gitS = git.add({args: "-u"});
-      gitS.once('data', function(newFile){
-        should.exist(newFile);
-        should.exist('test/.git/objects/');
-        done();
-      });
-      gitS.write(fakeFile);
-      gitS.end();
-    });
-
-    it('should add a Remote to the git repo - with options', function(done) {
-
-      git.addRemote('origin', 'https://github.com/stevelacy/git-test', {cwd:"./test/", args: "--no-tag"} , function(){
-        should.exist('test/.git/');
-          String(fs.readFileSync('test/.git/config').toString('utf8')).should.match(/https:\/\/github.com\/stevelacy\/git-test/);
-          done();
-      });
-
-    });
-/*
-    it('should pull from the remote repo - with options', function(done) {
-      git.pull('origin', 'master', {cwd: "./test/", args: "-v"}, function(){
-        should.exist('./test/.git/refs/heads/master');
-        done();
-      });
-    });
-*/
-
-    it('should commit a file to the repo - with options', function(done) {
-
-      var fakeFile = new gutil.File({
-        base: 'test/',
-        cwd: 'test/',
-        path: path.join(__dirname, 'test.js'),
-        contents: new Buffer(fs.readFileSync('./test/test.js'))
-      });
-      var gitS = git.commit('initial commit', {args: "-v"});
-      gitS.once('data', function(newFile){
-        setTimeout(function(){
-          String(fs.readFileSync(testCommit).toString('utf8')).should.match(/initial commit/);
-        }, 100);
-        done();
-      });
-      gitS.write(fakeFile);
-      gitS.end();
-
-    });
-
-
-    it('should create a new branch - with options', function(done){
-      git.branch("testBranch", {cwd: "./test/", args: "--track"}, function(){
-        should.exist('test/.git/refs/heads/testBranch');
-        done();
-      });
-    });
-
-    it('should merge branches', function(done){
-      git.merge("testBranch", {cwd: "./test/"}, function(){
-        setTimeout(function(){
-          String(fs.readFileSync(testCommit).toString('utf8')).should.match(/initial commit/);
-        }, 100);
-        done();
-      });
-    });
+    */
 
 
     it('should rm a file', function(done) {
