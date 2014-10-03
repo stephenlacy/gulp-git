1224ffa0971360048be3b02bf6f1665a30d2fe22
Steve Lacy <me@slacy.me>
2014-10-02 21:15:37 -0700

    update fn call

--- test/main.js
+++ test/main.js
@@ -3,6 +3,7 @@
 var path = require('path');
 var rimraf = require('rimraf');
 var gutil = require('gulp-util');
+var git = require('../');
 
 // just so this file is clean
 var util = require('./_util');
@@ -10,11 +11,10 @@ var util = require('./_util');
 // omit logging
 gutil.log = function(){};
 
-var git = require('../');
 
 describe('gulp-git', function(){
 
-  var testSuite = util.testSuite;
+  var testSuite = util.testSuite();
 
   testSuite.forEach(function(file){
     var suite = path.basename(file, path.extname(file));
