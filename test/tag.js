72c823dd391e0414333f7e66efc1211ac5c252e9
Steve Lacy <me@slacy.me>
2016-04-05 09:08:15 -0700

    fix error with stdout in callback - close #110

--- test/tag.js
+++ test/tag.js
@@ -11,14 +11,17 @@ module.exports = function(git, testFiles, testCommit){
   // These must be run on a system which has git installed
   // no pull delay, and has git configured.
 
-
   it('should tag a version of the repo', function(done) {
-
     git.tag('v1.2.3', 'message', {cwd: './test/'}, function() {
       should.exist('test/.git/refs/tags/v1.2.3');
       done();
     });
-
   });
 
+  it('should not throw an error on success', function(done) {
+    git.tag('v2', 'message', {cwd: './test/'}, function(err) {
+      should.not.exist(err);
+      done();
+    });
+  });
 };
