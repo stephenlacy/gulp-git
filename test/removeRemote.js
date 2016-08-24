9671f9334432f3cf2ca6a67c729ca48043b7841c
Francis Bergin <bergin.francis@gmail.com>
2016-08-24 11:51:18 -0400

    Fix file exist test for removeRemote test

--- test/removeRemote.js
+++ test/removeRemote.js
@@ -11,11 +11,13 @@ module.exports = function(git, util){
   it('should remove the Remote origin from the git repo', function(done) {
     var opt = {cwd: './test/repo/'};
     git.removeRemote('origin', opt, function(){
-      should.exist('./test/repo/.git/');
-      fs.readFileSync('./test/repo/.git/config')
-        .toString('utf8')
-        .should.not.match(/https:\/\/github.com\/stevelacy\/git-test/);
-      done();
+      fs.stat('./test/repo/.git/', function(err, stats){
+        should.not.exist(err);
+        fs.readFileSync('./test/repo/.git/config')
+          .toString('utf8')
+          .should.not.match(/https:\/\/github.com\/stevelacy\/git-test/);
+        done();
+      });
     });
   });
 
