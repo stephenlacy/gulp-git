9315bc070dbda47c6e0ae470922c8da7f613f0bb
Francis Bergin <bergin.francis@gmail.com>
2016-08-24 11:24:39 -0400

    Fix file exist test for branch test

--- test/branch.js
+++ test/branch.js
@@ -11,21 +11,25 @@ module.exports = function(git, util){
   it('should create a new branch', function(done){
     var opt = {cwd: './test/repo/'};
     git.branch('testBranch', opt, function(){
-      should.exist('test/repo/.git/refs/heads/testBranch');
-      done();
+      fs.stat('test/repo/.git/refs/heads/testBranch', function(err, stats) {
+        should.not.exist(err);
+        done();
+      });
     });
   });
 
   it('should create new branch, checkout and return its name', function(done){
     var opt = {cwd: './test/repo/'};
     git.branch('anotherBranch', opt, function(){
-      should.exist('test/repo/.git/refs/heads/anotherBranch');
-      git.checkout('anotherBranch', opt, function(){
-        opt = {args: '--abbrev-ref HEAD', cwd: opt.cwd};
-        git.revParse(opt, function (err, branch) {
-          branch.should.equal('anotherBranch');
-          should.not.exist(err);
-          done();
+      fs.stat('test/repo/.git/refs/heads/anotherBranch', function(err, stats) {
+        should.not.exist(err);
+        git.checkout('anotherBranch', opt, function(){
+          opt = {args: '--abbrev-ref HEAD', cwd: opt.cwd};
+          git.revParse(opt, function (err, branch) {
+            branch.should.equal('anotherBranch');
+            should.not.exist(err);
+            done();
+          });
         });
       });
     });
